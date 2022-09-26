c34bf54ae35d1dfa06c768ff2d6cb9fae2a55b07
William Charlton <will_is_here@icloud.com>
2022-09-26 13:43:48 +0100

    Initial solution - Sort list - Calculate middle - Print

--- median.py
+++ median.py
@@ -5,8 +5,10 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
-        break
-print(numbers)
+        numbers.sort() # Sort the list
+        middle = len(numbers) // 2 # Get middle index
+        print(numbers[middle]) # Print value at middle index
